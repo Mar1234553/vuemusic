b68a3e8a141ea9bb0a048123667d8d77a3d3660d
zhumm <732618537@qq.com>
2023-05-25 18:09:47 +0800

    23-5-25

---
+++ musicobj02/src/components/IconList.vue
@@ -0,0 +1,11 @@
+<template lang="">
+    <div>
+        我是IconList
+    </div>
+</template>
+
+<script>
+export default {
+    name:"iconlist"
+}
+</script>
---
+++ musicobj02/src/components/MusicList.vue
@@ -0,0 +1,11 @@
+<template lang="">
+    <div>
+        我是MusicList
+    </div>
+</template>
+
+<script>
+export default {
+    name:"musiclist"
+}
+</script>
